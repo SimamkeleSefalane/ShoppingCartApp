d02ac8b0b3ff915a15c199d05619fb99b6ff6714
SimamkeleSefalane <102727091+SimamkeleSefalane@users.noreply.github.com>
2022-04-28 10:40:18 +0200

    Added New Class

---
+++ ShoppingCartApp/ShoppingCartApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using ShoppingLibrary;
+using System;
+
+namespace ShoppingCartApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShoppingCart test = new ShoppingCart();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
